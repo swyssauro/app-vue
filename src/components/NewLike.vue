cf07fb6b843710a587c3f89693c273b34649e75c
sωıтcн <40813662+swyssauro@users.noreply.github.com>
2020-08-31 22:00:50 -0300

    🥱 Initial commit

---
+++ src/components/NewLike.vue
@@ -0,0 +1,38 @@
+<template>
+<section>
+    <h1 v-if="prive === true">Level 10 :3</h1>
+    <h1 v-if="likes > 0 "> This video has {{ likes }} </h1>
+    <p v-else></p>
+    <button v-on:click.capture="privety" v-on:click="likeAndSubs">Like & Subs</button>
+</section>
+</template>
+<script>
+export default {
+
+}
+</script>
+<script>
+export default {
+
+    data() {
+        return {
+            likes: 1,
+            prive: false
+        }
+    },
+
+    methods: {
+        likeAndSubs() {
+            this.likes++;
+        },
+
+        privety() {
+            if(this.likes > 11 ){
+                this.prive === false; {
+                    this.prive = true;
+                }
+            }
+        }
+    }
+}
+</script>
